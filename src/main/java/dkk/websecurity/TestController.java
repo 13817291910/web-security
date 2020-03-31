1b70dd77808e7ae53e6b2cf1becb3e4fe76896e9
kandi <13817291910@163.com>
2020-03-31 13:24:40 +0800

    add responsebody

--- src/main/java/dkk/websecurity/TestController.java
+++ src/main/java/dkk/websecurity/TestController.java
@@ -4,12 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
     private static Logger logger = LoggerFactory.getLogger("TestController");
     @RequestMapping("/")
-    void home(String flag){
+    @ResponseBody
+    String home(String flag){
         logger.info("flag=" + flag);
+        return "flag=" + flag;
     }
 }
