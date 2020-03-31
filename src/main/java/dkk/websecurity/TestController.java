d262745f8aa498d76dd3306c64323b88aad4275d
kandi <13817291910@163.com>
2020-03-31 12:51:29 +0800

    first commit

---
+++ src/main/java/dkk/websecurity/TestController.java
@@ -0,0 +1,15 @@
+package dkk.websecurity;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+    private static Logger logger = LoggerFactory.getLogger("TestController");
+    @RequestMapping("/")
+    void home(String flag){
+        logger.info("flag=" + flag);
+    }
+}
